664fe7a23141bed8d2caae27534ef979b21ea412
precious123-gifted <58713062+precious123-gifted@users.noreply.github.com>
2022-06-20 23:01:36 +0100

    Add files via upload

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 5-raise_exception.py
+# Ojo Obafemi David
+
+def raise_exception():
+    """Raise a TypeError exception."""
+    raise TypeError
---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 6-raise_exception_msg.py
+# Ojo Obafemi David
+
+def raise_exception_msg(message=""):
+    """Raise a NameError exception with a message."""
+    raise NameError(message)

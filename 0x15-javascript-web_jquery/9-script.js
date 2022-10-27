5fe94fa83f003b6ba1e65701acf5447a12d108b8
precious123-gifted <58713062+precious123-gifted@users.noreply.github.com>
2022-10-27 05:46:18 +0100

    jQUERY commit

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+  $.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data, status) {
+    if (status === 'success') {
+      $('DIV#hello').text(data.hello);
+    }
+  });
+});
